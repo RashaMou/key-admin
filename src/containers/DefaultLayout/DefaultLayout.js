e2ddf17465b6258c0c95016f1b92e6b332eb19c4
Rasha Moumneh <rasha@fastmail.com>
2020-06-09 17:58:59 -0400

    remove extraneous comments

--- src/containers/DefaultLayout/DefaultLayout.js
+++ src/containers/DefaultLayout/DefaultLayout.js
@@ -1,4 +1,4 @@
-import React, { Component, Suspense } from "react";
+import React, { Component } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import * as router from "react-router-dom";
 import { Container } from "reactstrap";
@@ -15,23 +15,13 @@ import {
   AppBreadcrumb2 as AppBreadcrumb,
   AppSidebarNav2 as AppSidebarNav,
 } from "@coreui/react";
-// sidebar nav config
 import navigation from "../../sidebarNav";
-// routes config
 import routes from "../../routes";
 import DefaultAside from "./DefaultAside";
 import DefaultFooter from "./DefaultFooter";
 import DefaultHeader from "./DefaultHeader";
 
-// const DefaultAside = React.lazy(() => import("./DefaultAside"));
-// const DefaultFooter = React.lazy(() => import("./DefaultFooter"));
-// const DefaultHeader = React.lazy(() => import("./DefaultHeader"));
-
 class DefaultLayout extends Component {
-  // loading = () => (
-  //   <div className="animated fadeIn pt-1 text-center">Loading...</div>
-  // );
-
   signOut(e) {
     e.preventDefault();
     this.props.history.push("/login");
@@ -41,9 +31,7 @@ class DefaultLayout extends Component {
     return (
       <div className="app">
         <AppHeader fixed>
-          {/* <Suspense fallback={this.loading()}> */}
           <DefaultHeader />
-          {/* </Suspense> */}
         </AppHeader>
         <div className="app-body">
           <AppSidebar fixed display="lg">
@@ -55,14 +43,12 @@ class DefaultLayout extends Component {
               {...this.props}
               router={router}
             />
-            {/* </Suspense> */}
             <AppSidebarFooter />
             <AppSidebarMinimizer />
           </AppSidebar>
           <main className="main">
             <AppBreadcrumb appRoutes={routes} router={router} />
             <Container fluid>
-              {/* <Suspense fallback={this.loading()}> */}
               <Switch>
                 {routes.map((route, idx) => {
                   return route.component ? (
@@ -77,19 +63,14 @@ class DefaultLayout extends Component {
                 })}
                 <Redirect from="/" to="/dashboard" />
               </Switch>
-              {/* </Suspense> */}
             </Container>
           </main>
           <AppAside fixed>
-            {/* <Suspense fallback={this.loading()}> */}
             <DefaultAside />
-            {/* </Suspense> */}
           </AppAside>
         </div>
         <AppFooter>
-          {/* <Suspense fallback={this.loading()}> */}
           <DefaultFooter />
-          {/* </Suspense> */}
         </AppFooter>
       </div>
     );
