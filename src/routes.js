2a7ff2b09800302e242e080c91ca2f1aaeda7bc3
Rasha Moumneh <rasha@fastmail.com>
2020-06-09 18:21:54 -0400

    Moar cleanup

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,4 @@
 import Dashboard from "./views/Dashboard";
-import CoreUIIcons from "./views/Icons/CoreUIIcons";
-import Flags from "./views/Icons/Flags";
-import SimpleLineIcons from "./views/Icons/SimpleLineIcons";
 import Users from "./views/Users/components/Users";
 import User from "./views/Users/components/User";
 import StrikeReports from "./views/StrikeReports/StrikeReports";
@@ -11,14 +8,6 @@ import Donations from "./views/Donations/Donations";
 const routes = [
   { path: "/", exact: true, name: "Home" },
   { path: "/dashboard", name: "Dashboard", component: Dashboard },
-  { path: "/icons", exact: true, name: "Icons", component: CoreUIIcons },
-  { path: "/icons/coreui-icons", name: "CoreUI Icons", component: CoreUIIcons },
-  { path: "/icons/flags", name: "Flags", component: Flags },
-  {
-    path: "/icons/simple-line-icons",
-    name: "Simple Line Icons",
-    component: SimpleLineIcons,
-  },
   { path: "/users", exact: true, name: "Users", component: Users },
   { path: "/users/:id", exact: true, name: "User Details", component: User },
   {
